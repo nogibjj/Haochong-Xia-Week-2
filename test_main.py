a0010335990709f47fb0f508273185f0450a46bb
hxia5 <hx96@duke.edu>
2023-09-08 13:00:01 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,8 @@
-from main import get_sum
+from main import Describe
+import pandas as pd
+data = pd.read_csv("https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv")
 
-
-def test_sum():
-    assert get_sum([1, 2, 3]) == 6
+def Test_Describe(data):
+    result = Describe(data)
+    assert result.loc['count', 'Shape_Leng'] == 1189.0
+    assert result.loc['mean', 'Shape_Area'] == 228153453.41946846
