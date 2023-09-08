4afdfb7508241856aff879da1351db8736aef1e6
hxia5 <hx96@duke.edu>
2023-09-08 12:52:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-def get_sum(l):
-    return sum(l)
+import pandas as pd
+data = pd.read_csv("https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv")
+def Describe(df): 
+    return df.describe()    
