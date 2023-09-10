93d58deac6491141c82b0598fc5ae946c6c8e42e
hxia5 <hx96@duke.edu>
2023-09-10 15:08:52 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,11 @@
-from main import Describe
+from main import (
+    Describe,
+    get_median,
+    PlotShapeLeng,
+    Summary,
+)
 import pandas as pd
 
-# data = pd.read_csv("https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv")
 example_csv = "https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv"
 
 def test_descirbe():
@@ -9,3 +13,19 @@ def test_descirbe():
     result = Describe(data)
     assert result.loc['count', 'Shape_Leng'] == 1189.0
     assert result.loc['mean', 'Shape_Area'] == 228153453.41946846
+    assert result.loc['mean', 'Shape_Leng'] == 60622.470193
+    assert result.loc['std', 'Shape_Area'] == 7.611513e+06
+    assert result.loc['std', 'Shape_Area'] == 1094.006226
+
+def test_median():
+    data = pd.read_csv(example_csv)
+    assert data['Shape_Leng'].median() == 60896.5746
+    assert data['Shape_Area'].median() == 230060526.252
+
+def test_viz_iris_and_general():
+    PlotShapeLeng(example_csv)
+
+
+def test_generate_summary_report():
+    Summary(example_csv)
+
